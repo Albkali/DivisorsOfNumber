ea34301033b79151eb5685e8fdb19dedfa359f75
Yasser Albkali <Yasser@albkali.com>
2020-02-06 03:09:34 +0300

    ADVANCED DATABASE PROJECT

---
+++ YASSER/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 268)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(475, 25)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = ".."
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 10.8!)
+        Me.Button1.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Button1.Location = New System.Drawing.Point(12, 341)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(475, 37)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Choose the database"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Font = New System.Drawing.Font("Tahoma", 9.25!, System.Drawing.FontStyle.Bold)
+        Me.TextBox2.ForeColor = System.Drawing.Color.Brown
+        Me.TextBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.TextBox2.Location = New System.Drawing.Point(12, 299)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(475, 36)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.Text = "select all;"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button2
+        '
+        Me.Button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Button2.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.Button2.BackgroundImage = Global.YASSER.My.Resources.Resources._5895d31bcba9841eabab607f
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.Button2.Enabled = False
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 10.8!)
+        Me.Button2.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Button2.Location = New System.Drawing.Point(12, 384)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(475, 41)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Execute the query"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.AntiqueWhite
+        Me.TextBox3.Font = New System.Drawing.Font("Tahoma", 9.8!)
+        Me.TextBox3.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.TextBox3.Location = New System.Drawing.Point(12, 14)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox3.Size = New System.Drawing.Size(475, 248)
+        Me.TextBox3.TabIndex = 4
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "database"
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Label1.Location = New System.Drawing.Point(12, 428)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(475, 117)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Label4.Location = New System.Drawing.Point(175, 549)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(102, 21)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Created by"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.Label5.ForeColor = System.Drawing.Color.Blue
+        Me.Label5.Location = New System.Drawing.Point(175, 570)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(102, 29)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Yasser Albkali"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.CadetBlue
+        Me.ClientSize = New System.Drawing.Size(499, 598)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "DBMS"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "YASSERDBMS | Yasser Albkali"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ YASSER/Form1.vb
@@ -0,0 +1,168 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Function dbms(ByVal query As String)
+        Try
+            Dim db As String() = IO.File.ReadAllLines(TextBox1.Text)
+            Dim ColumnsCount As Integer = CountCharacter(db(0), ",")
+            Dim Columns As String() = db(0).Split(",")
+            Dim itemindex As Integer = 0
+            Dim condition = False
+            Dim value As String = ""
+            If query.Contains("select all") Then
+                If query.StartsWith("where") Then
+                    Dim patt As String = "select all where (.*?) = (.*?);"
+                    Dim rx As Regex = New Regex(patt)
+                    Dim colname As String = rx.Match(query).Groups(1).ToString
+                    value = rx.Match(query).Groups(2).ToString
+                    condition = True
+                    itemindex = Array.IndexOf(Columns, colname)
+                End If
+                For i As Integer = 0 To ColumnsCount
+                    If (i <> ColumnsCount) Then
+                        TextBox3.AppendText(Columns(i) & " | ")
+                    Else
+                        TextBox3.AppendText(Columns(i) & vbNewLine)
+                    End If
+                Next
+                For i As Integer = 1 To db.Length
+                    Dim rows As String() = db(i).Split(",")
+                    If condition = True Then
+                        If rows(itemindex) = value Then
+                            For j As Integer = 0 To ColumnsCount
+                                If (j <> ColumnsCount) Then
+                                    TextBox3.AppendText(rows(j) & " | ")
+                                Else
+                                    TextBox3.AppendText(rows(j) & " | " & vbNewLine)
+                                End If
+                            Next
+                        End If
+                    Else
+                        For j As Integer = 0 To ColumnsCount
+                            If (j <> ColumnsCount) Then
+                                TextBox3.AppendText(rows(j) & " | ")
+                            Else
+                                TextBox3.AppendText(rows(j) & " | " & vbNewLine)
+                            End If
+                        Next
+                    End If
+
+                Next
+            ElseIf query.StartsWith("singleselect") Then
+                Dim patt As String = Nothing
+                If query.Contains("where") Then
+                    patt = "singleselect (.*?) where"
+                Else
+                    patt = "singleselect (.*?);"
+                End If
+                Dim rx As Regex = New Regex(patt)
+                Dim column As String = rx.Match(query).Groups(1).ToString
+                Dim itemindexz As Integer = Array.IndexOf(Columns, column)
+                If query.Contains("where") Then
+                    Dim patts As String = "singleselect " & column & " where (.*?) = (.*?);"
+                    Dim rxs As Regex = New Regex(patts)
+                    Dim colname As String = rxs.Match(query).Groups(1).ToString
+                    value = rxs.Match(query).Groups(2).ToString
+                    condition = True
+                    itemindex = Array.IndexOf(Columns, colname)
+                End If
+                TextBox3.AppendText(column & " ----- " & vbNewLine)
+                For i As Integer = 1 To db.Length
+                    Dim rows As String() = db(i).Split(",")
+                    If condition = True Then
+                        If rows(itemindex) = value Then
+                            TextBox3.AppendText(rows(itemindexz) & vbNewLine)
+                        End If
+                    Else
+                        TextBox3.AppendText(rows(itemindexz) & vbNewLine)
+                    End If
+                Next
+            ElseIf query.StartsWith("insert") Then
+                query = query.Replace("insert ", "")
+                If CountCharacter(query, ",") <> ColumnsCount Then
+                    MsgBox("You should insert " & ColumnsCount + 1 & " values only")
+                Else
+                    My.Computer.FileSystem.WriteAllText(TextBox1.Text, query & vbNewLine, True)
+                    TextBox3.AppendText("Executed successfully." & vbNewLine)
+                End If
+            ElseIf query.StartsWith("delete") Then
+                Dim patts As String = "delete where (.*?) = (.*?);"
+                Dim rxs As Regex = New Regex(patts)
+                Dim colname As String = rxs.Match(query).Groups(1).ToString
+                value = rxs.Match(query).Groups(2).ToString
+                condition = True
+                itemindex = Array.IndexOf(Columns, colname)
+                For i As Integer = 1 To db.Length
+                    Dim rows As String() = db(i).Split(",")
+                    If rows(itemindex) = value Then
+                        Dim lines As List(Of String) = db.ToList
+                        lines.RemoveAt(i)
+                        System.IO.File.WriteAllLines(TextBox1.Text, lines)
+                        TextBox3.AppendText("Deleted .." & vbNewLine)
+                        Exit For ' remove it to delete all
+                    End If
+                Next
+            ElseIf query.StartsWith("update") Then
+                Dim patts As String = "update (.*?) value (.*?) to (.*?);"
+                Dim rxs As Regex = New Regex(patts)
+                Dim colname As String = rxs.Match(query).Groups(1).ToString
+                value = rxs.Match(query).Groups(2).ToString
+                Dim newvalue = rxs.Match(query).Groups(3).ToString
+                condition = True
+                itemindex = Array.IndexOf(Columns, colname)
+                For i As Integer = 1 To db.Length
+                    Dim rows As String() = db(i).Split(",")
+                    If rows(itemindex) = value Then
+                        db(i) = db(i).Replace(value, newvalue)
+                        System.IO.File.WriteAllLines(TextBox1.Text, db)
+                        TextBox3.AppendText("Updated .." & vbNewLine)
+                        Exit For ' remove it to update all
+                    End If
+                Next
+            End If
+        Catch ex As Exception
+        End Try
+    End Function
+
+    Public Function CountCharacter(ByVal value As String, ByVal ch As Char) As Integer
+        Dim cnt As Integer = 0
+        For Each c As Char In value
+            If c = ch Then
+                cnt += 1
+            End If
+        Next
+        Return cnt
+    End Function
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+        TextBox1.Text = OpenFileDialog1.FileName
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox3.Clear()
+        dbms(TextBox2.Text)
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label4.Click, Label5.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+
+    End Sub
+End Class
